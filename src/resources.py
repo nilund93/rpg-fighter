6504105269fa3ea971e26da4662db4817d4d5731
Niclas Lund <niclas.lund@telia.com>
2022-02-15 15:00:44 +0100

    removed healing when damage is lower than armor

--- src/resources.py
+++ src/resources.py
@@ -9,12 +9,13 @@ class Character:
     def __str__(self) -> str:
         return f"Name: {self.name}\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor}"
     
-    def attack(self):
+    def damage(self):
         return self.attack
     
     def take_damage(self, damage):
         relative_damage = damage - self.armor
-        self.health -= relative_damage
+        if relative_damage > 0:
+            self.health -= relative_damage
         if self.health < 0: self.health = 0
 
     def get_health(self):
@@ -30,12 +31,13 @@ class Goblin:
     def __str__(self) -> str:
         return f"Goblin\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor}"
 
-    def attack(self):
+    def damage(self):
         return self.attack
     
     def take_damage(self, damage):
         relative_damage = damage - self.armor
-        self.health -= relative_damage
+        if relative_damage > 0:
+            self.health -= relative_damage
         if self.health < 0: self.health = 0
 
     def get_health(self):
