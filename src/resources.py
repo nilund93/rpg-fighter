1e2010f85a462ecfa50f064f791deae93a5b99d4
Niclas Lund <niclas.lund@telia.com>
2022-02-15 14:42:48 +0100

    added fighting methods

--- src/resources.py
+++ src/resources.py
@@ -1,6 +1,3 @@
-from mimetypes import init
-
-
 class Character:
     
     def __init__(self, name, health, attack, armor):
@@ -11,5 +8,37 @@ class Character:
         
     def __str__(self) -> str:
         return f"Name: {self.name}\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor}"
+    
+    def attack(self):
+        return self.attack
+    
+    def take_damage(self, damage):
+        relative_damage = damage - self.armor
+        self.health -= relative_damage
+        if self.health < 0: self.health = 0
+
+    def get_health(self):
+        return self.health
+
+class Goblin:
+    
+    def __init__(self, health, attack, armor):
+        self.health = health
+        self.attack = attack
+        self.armor = armor
         
+    def __str__(self) -> str:
+        return f"Goblin\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor}"
+
+    def attack(self):
+        return self.attack
+    
+    def take_damage(self, damage):
+        relative_damage = damage - self.armor
+        self.health -= relative_damage
+        if self.health < 0: self.health = 0
+
+    def get_health(self):
+        return self.health
+    
     
