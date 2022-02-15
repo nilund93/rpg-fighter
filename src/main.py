3835ebdbfc4670cf6187e71252e627ad7772e21a
Niclas Lund <niclas.lund@telia.com>
2022-02-15 15:12:08 +0100

    added critical damage

--- src/main.py
+++ src/main.py
@@ -14,14 +14,14 @@ if __name__ == "__main__":
     print("=========FIGHT=========")
     while nemy.get_health() != 0 and goblin_one.get_health != 0:
         print(f"Round {fight_round}")
-        nemy_attack = nemy.damage()
+        nemy_attack = nemy.damage()*randint(1,3)
         goblin_one.take_damage(nemy_attack)
         if(goblin_one.get_health() == 0):
             print("Goblin has died.")
             break
         else:
             print(f"Goblin has {goblin_one.get_health()} hp left.")
-            goblin_attack = goblin_one.damage()
+            goblin_attack = goblin_one.damage()*randint(1,4)
             nemy.take_damage(goblin_attack)
             print(f"Nemy has {nemy.get_health()} hp left.")
             if(nemy.get_health() == 0): print("Nemy has died.")
